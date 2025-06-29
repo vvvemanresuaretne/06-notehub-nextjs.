70f197edbb05f97242e0995b75d9c28461b7ecad
vvvemanresuaretne <vvvasilkovskyi@gmail.com>
2025-06-29 19:31:33 +0200

    update

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { HydrationBoundary, dehydrate, QueryClient } from '@tanstack/react-query';
 import NoteDetailsClient from './NoteDetails.client';
-import { getSingleNote } from '@/lib/api';
+import { getSingleNote } from '@/app/lib/api';
 import type { ReactElement } from 'react';
 
 interface NoteDetailsProps {
