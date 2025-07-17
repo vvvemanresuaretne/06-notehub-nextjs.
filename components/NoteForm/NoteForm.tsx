9bc70ed2df181f8e14050edf99ae701f1210acd6
vvvemanresuaretne <vvvasilkovskyi@gmail.com>
2025-07-17 11:04:36 +0200

    update

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -8,7 +8,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { useNoteDraft } from "@/lib/store/noteStore";
-import { createNote } from "@/lib/api/clientApi";
+import { createNote } from "@/lib/api/api";
 
 
 const FormSchema = Yup.object({
--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -4,7 +4,7 @@ import css from "./NoteList.module.css";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import Link from "next/link";
-import { deleteNote } from "@/lib/api/clientApi";
+import { deleteNote } from "@/lib/api/api";
 
 type NoteListProps = {
   notes: Note[];
