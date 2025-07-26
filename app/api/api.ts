47bf0e1e6bf0da3027008eabb6baa03c928bf551
vvvemanresuaretne <vvvasilkovskyi@gmail.com>
2025-07-26 08:52:16 +0200

    update

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
-export const nextServer = axios.create({
-  baseURL,
+export const api = axios.create({
+  baseURL: "https://notehub-api.goit.study",
   withCredentials: true,
 });
